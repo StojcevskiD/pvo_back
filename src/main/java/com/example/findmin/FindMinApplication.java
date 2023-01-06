ddd175a1e0afe67cfe02746b8e3e8d82047a385a
StojcevskiD <dimitar.stojcevski@hotmail.com>
2023-01-06 23:03:48 +0100

    remove comments

--- src/main/java/com/example/findmin/FindMinApplication.java
+++ src/main/java/com/example/findmin/FindMinApplication.java
@@ -12,31 +12,7 @@ import java.util.Scanner;
 @SpringBootApplication
 public class FindMinApplication {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         SpringApplication.run(FindMinApplication.class, args);
-        File newFile = new File("content.txt");
-
-//        if(newFile.createNewFile()) {
-//            FileWriter writer = new FileWriter("content.txt");
-//            for (int i = 0; i < 500000; i++) {
-//                writer.write((int) (Math.random() * 10000) + "\n");
-//            }
-//            writer.close();
-//        }
-
-        Scanner reader = new Scanner(newFile);
-        int minimum = reader.nextInt();
-        int line = 1;
-        int currentLine = 1;
-        while (reader.hasNextInt()) {
-            int number = reader.nextInt();
-            if (minimum > number) {
-                minimum = number;
-                line = currentLine;
-            }
-            currentLine++;
-        }
-        System.out.println("Minimum: " + minimum + ", line: " + line);
     }
-
 }
